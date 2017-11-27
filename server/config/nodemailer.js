a4f3f3a38491de65b62cdb216efacb2c5f2abba1
Harvey Chui <chuiharvey@gmail.com>
2017-11-26 17:31:07 -0800

    Forgot to re-add the files

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -18,18 +18,19 @@ const leagueEmail = data => {
   return new Promise((resolve, reject) => {
     console.log("works2");
     let replaced = ""
-    const html = `<p>Hello Chris,</br></br>
-    Please validate this league:</br></br>
-    <span style="padding-left:2em"></span>League's Name: %%leagueName%%</br>
-    <span style="padding-left:2em"></span>League Admin's Name: %%firstName%% %%lastName%%</br>
-    <span style="padding-left:2em"></span>League Admin's Email: <a href="#">%%email%%</a></br>
-    <span style="padding-left:2em"></span>League Admin's Phone Number: %%phoneNumber%%</br>
-    <span style="padding-left:2em"></span>City: %%city%%</br>
-    <span style="padding-left:2em"></span>State: %%state%%</br>
-    <span style="padding-left:2em"></span>Number of Coaches: %%numCoaches%%</br>
-    <span style="padding-left:2em"></span>Number of Players: %%numPlayers%%</br></br>
-    <span style="padding-left:2em"></span><a href="https://youthdraft.com/league/validate/%%JWT%%">Accept</a>
-    <span style="padding-left:4em"></span><a href="https://youthdraft.com/league/reject/%%JWT%%">Reject</a></p>`
+    const html = `<p><span style="padding-bottom:2em;display:block">Hello Chris,</span>
+    <span style="padding-bottom:2em;display:block">Please validate this league:</span>
+    <span style="padding-left:2em;display:block">League's Name: %%leagueName%%</span>
+    <span style="padding-left:2em;display:block">League Admin's Name: %%firstName%% %%lastName%%</span>
+    <span style="padding-left:2em;display:block">League Admin's Email: <a href="#">%%email%%</a></span>
+    <span style="padding-left:2em;display:block">League Admin's Phone Number: %%phoneNumber%%</span>
+    <span style="padding-left:2em;display:block">City: %%city%%</span>
+    <span style="padding-left:2em;display:block">State: %%state%%</span>
+    <span style="padding-left:2em;display:block">Number of Teams: %%numTeams%%</span>
+    <span style="padding-left:2em;display:block">Number of Coaches: %%numCoaches%%</span>
+    <span style="padding-left:2em;padding-bottom:2em;display:block">Number of Players: %%numPlayers%%</span>
+    <span style="padding-left:2em"></span><a href='https://youthdraft.com/league/validate/%%JWT%%'>Accept</a>
+    <span style="padding-left:4em"></span><a href='https://youthdraft.com/league/reject/%%JWT%%'>Reject</a></p>`
     const parts = html.split(/(\%\%\w+?\%\%)/g).map(function(v) {
       replaced = v.replace(/\%\%/g,"");
       return data[replaced] || replaced;
